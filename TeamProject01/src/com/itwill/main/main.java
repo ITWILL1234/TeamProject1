1d7d8df4d70915dace30dc9c85e64742ea81de46
niccnam <root.node.01@gmail.com>
2024-02-20 17:52:31 +0900

    add mainpage

--- TeamProject01/src/com/itwill/main/main.java
+++ TeamProject01/src/com/itwill/main/main.java
@@ -1,8 +1,12 @@
 package com.itwill.main;
 
+import java.util.Scanner;
+
 public class main {
 
 	public static void main(String[] args) {
+		int inputRL = 0;
+		Scanner scan = new Scanner(System.in);
 		System.out.println(""
 				+ "\n"
 				+ "███████╗    ███╗   ███╗ █████╗ ██╗     ██╗     \n"
@@ -15,8 +19,24 @@ public class main {
 				+ "");
 		System.out.println();
 		System.out.println("1. 회원가입 2. 로그인");
-		System.out.println("");
+		inputRL = scan.nextInt();
+		scan.nextLine();
+		
+		while (inputRL != 1 && inputRL != 2) {
+			System.out.println();
+			System.out.println("입력값이 올바르지 않습니다.");
+			inputRL = scan.nextInt();
+			scan.nextLine();
+		}
+		
+		if (inputRL == 1) {
+			
+			
+		} else if (inputRL == 2) {
+			System.out.println("로그인 창으로 넘어갑니다.");
+		}
 
 	}
 
 }
+
