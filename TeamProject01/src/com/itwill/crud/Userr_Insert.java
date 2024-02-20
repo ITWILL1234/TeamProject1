edc746a7dfc1f5819c086fcdf728f0756227579a
RTPC <hyhy9501@naver.com>
2024-02-20 16:42:11 +0900

    MergeTest1

--- TeamProject01/src/com/itwill/crud/Userr_Insert.java
+++ TeamProject01/src/com/itwill/crud/Userr_Insert.java
@@ -7,20 +7,38 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import com.itwill.user.vo.UserVO;
+import com.itwill.user.scanner.userscanner;
+
 public class Userr_Insert {
 
 	public static void main(String[] args) {
     	// ** ID : ADMIN, PW: admin **  ** 테이블명  USERR  **USE 예약어라 USERR로 설정 **
+		
+		String sql = "INSERT INTO USERR (EMAIL, PASSWORD, GENDER, FIRST_NAME, LAST_NAME, ADDRESS, CREATE_AT) "
+				   + "VALUES ('?', '?', '?', '?', '?','?', SYSDATE)";
+		
+		userscanner uc = new userscanner();
+		UserVO user = uc.register();
+		
         try (
             Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "ADMIN", "admin");
-            Statement stmt = conn.createStatement();
+            PreparedStatement pstmt = conn.prepareStatement(sql);
         ) {
-        	String sql = "INSERT INTO USERR (EMAIL, PASSWORD, GENDER, FIRST_NAME, LAST_NAME, ADDRESS, CREATE_AT) "
-        			   + "VALUES ('WDDDASSDA@ASD.COM', '1122', '남', '김', '철수','강남어딘가', SYSDATE)";
+        	int i = 1;
+        	pstmt.setString(i++, user.getEMAIL());
+        	pstmt.setString(i++, user.getPASSWORD());
+        	pstmt.setString(i++, user.getGENDER());
+        	pstmt.setString(i++, user.getFIRSTNAME());
+        	pstmt.setString(i++, user.getLASTNAME());
+        	pstmt.setString(i++, user.getADDRESS());
 		   
         	System.out.println("sql : " + sql);
-        	int result = stmt.executeUpdate(sql);
+        	int result = pstmt.executeUpdate(sql);
 			System.out.println("처리건수 : " + result);
+			if (result == 0) {
+				System.out.println("회원가입이 되지 않았습니다! 관리자에게 문의하세요!");
+			}
     
         } catch (SQLException e) {
             e.printStackTrace();
