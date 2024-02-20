610b0c89dd1bfc6d01f749714e08a772afbc5622
niccnam <root.node.01@gmail.com>
2024-02-20 17:02:00 +0900

    오류

--- TeamProject01/src/com/itwill/main/mainpage.java
+++ TeamProject01/src/com/itwill/main/mainpage.java
@@ -1,8 +1,12 @@
 package com.itwill.main;
 
-public class main {
+import java.util.Scanner;
+
+public class mainpage {
 
 	public static void main(String[] args) {
+		int inputRL = 0;
+		Scanner scan = new Scanner(System.in);
 		System.out.println(""
 				+ "\n"
 				+ "███████╗    ███╗   ███╗ █████╗ ██╗     ██╗     \n"
@@ -15,7 +19,22 @@ public class main {
 				+ "");
 		System.out.println();
 		System.out.println("1. 회원가입 2. 로그인");
-		System.out.println("");
+		inputRL = scan.nextInt();
+		scan.nextLine();
+		
+		while (inputRL != 1 && inputRL != 2) {
+			System.out.println();
+			System.out.println("입력값이 올바르지 않습니다.");
+			inputRL = scan.nextInt();
+			scan.nextLine();
+		}
+		
+		if (inputRL == 1) {
+			System.out.println("회원가입 창으로 넘어갑니다.");
+			
+		} else if (inputRL == 2) {
+			System.out.println("로그인 창으로 넘어갑니다.");
+		}
 
 	}
 
